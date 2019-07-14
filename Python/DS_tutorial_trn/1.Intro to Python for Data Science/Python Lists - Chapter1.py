132e5b80e7c997d3d6509d1f938846dc7f23cc1a
aniljadon <aniljadon.jadon@gmail.com>
2019-07-14 10:42:09 +0530

    no message

---
+++ Python/DS_tutorial_trn/1.Intro to Python for Data Science/Python Lists - Chapter1.py
@@ -0,0 +1,61 @@
+  #Calculations with variables
+
+
+# Create a variable savings
+savings = 100
+
+# Create a variable factor
+factor = 1.1
+
+# Calculate result
+result = savings * factor ** 7
+
+# Print out result
+print(result)
+
+
+# Other Variable Types
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+
+
+#Operations with other types
+# Several variables to experiment with
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of savings and factor to year1
+year1 = savings * factor
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+
+#Type conversion
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+
+
